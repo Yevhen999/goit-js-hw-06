1ef0ab8fa299f80e5b1cf09fb4b41310c44b995a
Yevhen <leshchenkoyevhen@gmail.com>
2022-09-05 21:02:06 +0300

    complete task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const items = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const oneItem = document.createElement("li");
+
+  let ingredient = ingredients[i];
+  oneItem.textContent = ingredient;
+  oneItem.classList.add("item");
+
+  items.push(oneItem);
+}
+
+console.log(items);
+const createdIngredients = document.querySelector("#ingredients");
+
+createdIngredients.append(...items);
