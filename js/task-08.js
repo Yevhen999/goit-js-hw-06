0922c3dbbe11fc6b083fe064d724356211d74fd0
Yevhen <leshchenkoyevhen@gmail.com>
2022-09-08 16:52:45 +0300

    completed task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const onSubmitRef = document.querySelector(".login-form");
 
+onSubmitRef.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const elementsForm = event.currentTarget.elements;
+  const email = elementsForm.email.value;
+  const password = elementsForm.password.value;
+
+  if (email === "" || password === "") {
+    alert("Всі поля повинні бути заповнені");
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+  // Alternative----------------------->
+  //------->   const formData = new FormData(event.currentTarget);
+
+  //   formData.forEach((value, name) => {
+  //     console.log(name, value);
+  //   });
+}
