91c397a4baffed0bcc7065ff7ef61f7a5433a99f
Yevhen <leshchenkoyevhen@gmail.com>
2022-09-09 13:25:36 +0300

    update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,38 +1,12 @@
-const numberOfCategories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${numberOfCategories.length}`);
+const numberOfCategories = document.querySelector("#categories");
+console.log(`Number of categories: ${numberOfCategories.children.length}`);
 console.log("");
 
-const listOfCategories = document.querySelector("#categories");
+const itemsOfCategories = document.querySelectorAll(".item");
 
-// Animals------------------------------
-
-const liOfAnimals = listOfCategories.firstElementChild;
-
-const titleAnimals = liOfAnimals.querySelector("h2");
-
-console.log(`Category: ${titleAnimals.textContent}`);
-
-const itemsOfAnimals = liOfAnimals.querySelectorAll("li");
-console.log(countItems(itemsOfAnimals));
-console.log("");
-// Products-----------------------
-const liOfProducts = liOfAnimals.nextElementSibling;
-
-const titleProducts = liOfProducts.querySelector("h2");
-console.log(`Category: ${titleProducts.textContent}`);
-
-const itemsOfProducts = liOfProducts.querySelectorAll("li");
-console.log(countItems(itemsOfProducts));
-console.log("");
-// // Technologies-----------------------
-const liOfTechnologies = liOfProducts.nextElementSibling;
-
-const titleTechnologies = liOfTechnologies.querySelector("h2");
-console.log(`Category: ${titleTechnologies.textContent}`);
-
-const itemsOfTechnologies = liOfTechnologies.querySelectorAll("li");
-console.log(countItems(itemsOfTechnologies));
-
-function countItems(itemsOf) {
-  return `Elements: ${itemsOf.length}`;
-}
+itemsOfCategories.forEach((item) => {
+  const itemsOfNames = item.querySelectorAll("li");
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${itemsOfNames.length}`);
+  console.log("");
+});
